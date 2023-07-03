b8ec7c1d616ceb34adff13525a1280f733cf9378
Bernardo Melo <chillsmeit@proton.me>
2023-07-03 21:31:20 +0100

    Create FakespotUS&EU.js

---
+++ FakespotUS&EU.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name     Fakespot Amazon
+// @version  2
+// @match    https://*.amazon.com/*
+// @match    https://*.amazon.de/*
+// @match    https://*.amazon.es/*
+// @match    https://*.amazon.fr/*
+// @match    https://*.amazon.it/*
+// @match    https://*.amazon.jp/*
+// @match    https://*.amazon.nl/*
+// @match    https://*.amazon.se/*
+// @match    https://*.amazon.co.uk/*
+// ==/UserScript==
+
+const url = "https://www.fakespot.com/analyze?url="+ encodeURIComponent(window.location)
+window.addEventListener('load', () => {
+
+  let ratings = document.getElementById("averageCustomerReviews_feature_div");
+  let a = document.createElement('a')
+  a.href = url
+  a.innerText = "Fakespot"
+  a.target="_blank"
+
+  let askPipe = document.createElement('span')
+  askPipe.className = "askPipe"
+  askPipe.innerText = "|"
+
+  ratings.append(askPipe)
+  ratings.append(a)
+
+}, false);
